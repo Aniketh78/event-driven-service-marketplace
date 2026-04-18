fc234a31d8248044ccae3d4fd9205641e442e022
Aniketh78 <231501014@rajalakshmi.edu.in>
2026-04-18 19:57:59 +0530

    Added Simple API gateway

---
+++ APIGateway/src/test/java/com/booking_micro/APIGateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.booking_micro.APIGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- AuthService/src/main/java/com/booking_micro/AuthService/dto/LoginRequest.java
+++ AuthService/src/main/java/com/booking_micro/AuthService/dto/LoginRequest.java
@@ -7,6 +7,7 @@ import lombok.Setter;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.NotNull;
 
 @Getter
 @Setter
@@ -21,4 +22,5 @@ public class LoginRequest {
     @NotBlank(message = "Password is required")
     @Size(min = 4, max = 100, message = "Password must be at least 4 characters")
     private String password;
+
 }
